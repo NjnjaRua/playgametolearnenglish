963e70aa5e488d6c39d42657d17b9d0a0ab97a1a
Truong Nguyen <nhattruong159@gmail.com>
2018-10-14 16:42:40 +0700

    add resources

---
+++ PlayGameToLearnEnglish/Assets/Scripts/AppMain.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AppMain : MonoBehaviour {
+    [SerializeField]
+    private Text titleName;
+
+    [SerializeField]
+    private Text txtPlay;
+
+    [SerializeField]
+    private Sprite spriteSoundOn;
+
+    [SerializeField]
+    private Sprite spriteSoundOff;
+
+    [SerializeField]
+    private Image imgSound;
+
+    bool isSoundOn;
+    bool isInit;
+
+    // Use this for initialization
+    void Start () {
+        OnInit();
+    }
+
+    void OnInit()
+    {
+        if (isInit)
+            return;
+        isInit = true;
+        OnupdateSound();
+    }
+
+    void OnupdateSound()
+    {
+        imgSound.sprite = (isSoundOn) ? spriteSoundOn : spriteSoundOff;
+    }
+
+    #region Function
+    public void OnPlay()
+    {
+        Debug.Log("OnPlay");
+    }
+
+    public void OnGetHighScore()
+    {
+        Debug.Log("OnGetHighScore");
+    }
+
+    public void OnGetFreeDiamond()
+    {
+        Debug.Log("OnGetFreeDiamond");
+    }
+
+    public void OnSetSound()
+    {
+        Debug.Log("OnSetSound");
+        isSoundOn = !isSoundOn;
+        OnupdateSound();
+    }
+
+    public void OnShare()
+    {
+        Debug.Log("OnShare");
+    }
+
+    public void OnRate()
+    {
+        Debug.Log("OnRate");
+    }
+    #endregion
+}
